05a0c98eaaff8149995646298a74da5a55397204
Laiacarrillo <Laiacarrilloes@gmail.com>
2025-04-30 12:53:58 -0500

    Initial commit with project structure (Day 1)

---
+++ src/main/java/main/java/com/carmotors/common/Address.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.carmotors.common;
+
+/**
+ *
+ * @author fashe
+ */
+
+public class Address {
+    private int id;
+    private String street;
+    private String city;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
---
+++ src/main/java/main/java/com/carmotors/services/Service.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main.java.com.carmotors.services;
+
+/**
+ *
+ * @author fashe
+ */
+
+public class Service {
+    private int id;
+    private String type;
+    private int vehicleId;
+    private String description;
+    private int estimatedTime;
+    private double laborCost;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    // Otros getters y setters...
+}
